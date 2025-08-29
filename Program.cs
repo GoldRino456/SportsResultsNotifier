d8388528b0a1cb68c8c2b64255b4e140f8fb26af
Ethan E. <eastwoode375@gmail.com>
2025-08-28 22:24:44 -0500

    Experimenting with Scraping

--- Program.cs
+++ Program.cs
@@ -1,4 +1,19 @@
-﻿using SportsResultsNotifier;
+﻿using HtmlAgilityPack;
+using SportsResultsNotifier;
 
-var email = new EmailData() { Body = "Body!", Subject = "Subject Line" };
-EmailManager.SendEmail(email);
+//var email = new EmailData() { Body = "Body!", Subject = "Subject Line" };
+//EmailManager.SendEmail(email);
+
+HtmlWeb web =  new HtmlWeb();
+HtmlDocument doc = web.Load("https://www.baseball-reference.com/teams/TEX/2025.shtml");
+
+var obj = doc.DocumentNode.SelectNodes("//*[@id=\"content\"]/div[3]/div"); ////*[@id="content"]/div[3]/div[1]/table //*[@id="content"]/div[3]/div[1]/table/tbody/tr[1]
+
+foreach (var item in obj)
+{
+    Console.WriteLine(item.SelectSingleNode("table/tbody/tr[1]").InnerText);
+    Console.WriteLine(item.SelectSingleNode("table/tbody/tr[2]").InnerText.Trim());
+    Console.WriteLine(item.SelectSingleNode("table/tbody/tr[3]").InnerText.Trim());
+    Console.Write("-----\n");
+}
+
